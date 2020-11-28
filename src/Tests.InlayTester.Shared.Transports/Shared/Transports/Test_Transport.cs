3575cae778b7bdfbcb3c488d9784f19998969557
Olaf Kober <olaf.kober@outlook.com>
2020-11-28 08:35:14 +0100

    replaced license headers

--- src/Tests.InlayTester.Shared.Transports/Shared/Transports/Test_Transport.cs
+++ src/Tests.InlayTester.Shared.Transports/Shared/Transports/Test_Transport.cs
@@ -1,6 +1,6 @@
 ﻿/* MIT License
  * 
- * Copyright (c) 2019, Olaf Kober
+ * Copyright (c) 2020, Olaf Kober
  * https://github.com/Amarok79/InlayTester.Shared.Transports
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-*/
+ */
 
 using System;
 using Common.Logging;
